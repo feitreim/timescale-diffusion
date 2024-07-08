bd66a5ec6b88bbe474a3db083c3d4a60f52eddce
finn eitreim <feitreim0@gmail.com>
2024-07-08 12:36:03 -0700

    dataloaders

---
+++ data/dali_loader.py
@@ -0,0 +1,257 @@
+import torch
+import os
+import pytorch_lightning as pl
+from typing import Tuple
+
+from nvidia.dali import pipeline_def
+import nvidia.dali.fn as fn
+import nvidia.dali.types as types
+import nvidia.dali.plugin.pytorch as pydali
+from torch.utils.data import DataLoader
+from einops import rearrange
+from datasets.utils import convert_timestamp_to_periodic
+
+"""
+Dali pipeline, allows gpu-accelerated dataloading
+Args:
+    - filenames: list[str]: list of the files for the dataloader.
+    - sequence_length: int: multi-frame size.
+    - output_size: list[int]: output dimensions for frames.
+    - buffer_size: int: size of the dataloading buffer.
+    - mf_distance: int: distance between multiframe-frames
+"""
+
+
+@pipeline_def
+def video_pipeline(
+    filenames,
+    sequence_length,
+    output_size,
+    buffer_size,
+    mf_distance,
+    shard_id,
+    num_shards,
+    name,
+    shuffle,
+):
+    frames, labels, frame_number = fn.readers.video_resize(
+        device="gpu",
+        additional_decode_surfaces=4,
+        filenames=filenames,
+        sequence_length=sequence_length,
+        shard_id=shard_id,
+        num_shards=num_shards,
+        random_shuffle=shuffle,
+        initial_fill=buffer_size,
+        enable_frame_num=True,
+        stride=mf_distance,
+        size=output_size,
+        mode="stretch",
+        minibatch_size=32,
+        read_ahead=True,
+        prefetch_queue_depth=2,
+        name=name,
+        labels=[],
+    )
+
+    # Image Transforms
+    reordered = fn.transpose(frames, perm=[0, 3, 1, 2])  # SCHW
+    scale = types.Constant(255)
+    normalized = reordered / scale
+
+    return normalized, labels, frame_number
+
+
+class DALIDataset(torch.utils.data.IterableDataset):
+    """
+    Iterator Style pytorch dataset that uses a NVIDIA DALI pipeline to load video data
+    quickly with gpu acceleration. Also provides multi-frame context.
+
+    Args:
+        - video_file_paths: (List[str]): A list of all the paths to the data.
+        - sequence_length: int: number of total frames for multi-frame context.
+        - batch_size: int: batch size, number of multi-frame sets.
+        - num_threads: int: number of cpu(?) threads for dataloading.
+        - output_size: list[int, int]: Pipeline output image size.
+        - buffer_size: int: size of the preloaded buffer
+        - mf_distance: int: distance between sequence frames.
+        - device_id: int: device id of the cuda device to run the dataloader on.
+        - num_devices: int: number of total devices
+        - shard_id: int: id of the shard.
+    """
+
+    def __init__(
+        self,
+        video_file_paths,
+        sequence_length,
+        batch_size,
+        num_threads,
+        output_size,
+        buffer_size,
+        mf_distance,
+        num_devices=1,
+        device_id=0,
+        shard_id=0,
+        shuffle=True,
+    ):
+        super().__init__()
+        self.name = f"Reader{device_id}"
+        pipe = video_pipeline(
+            filenames=video_file_paths,
+            sequence_length=sequence_length,
+            batch_size=batch_size,
+            num_threads=num_threads,
+            device_id=device_id,
+            output_size=output_size,
+            buffer_size=buffer_size,
+            mf_distance=mf_distance,
+            shard_id=shard_id,
+            num_shards=num_devices,
+            name=self.name,
+            shuffle=shuffle,
+            prefetch_queue_depth=4,
+            set_affinity=True,
+        )
+        self.pipeline = pydali.DALIGenericIterator(
+            pipe, output_map=["frames", "labels", "frame_number"], reader_name=self.name
+        )
+        self.video_timestamps = []
+        self.compute_frame_timestamps(video_file_paths)
+
+        hw = sequence_length // 2
+        self.offsets = [(i - hw) * mf_distance for i in range(sequence_length)]
+        self.sq_len = sequence_length
+
+    def __iter__(self):
+        for data in enumerate(self.pipeline):
+            frames, labels, frame_number = (
+                data[1][0]["frames"],
+                data[1][0]["labels"],
+                data[1][0]["frame_number"],
+            )
+
+            frames = rearrange(frames, "B S C H W -> (B S) C H W")
+            timestamps = torch.empty((frames.shape[0], 1, 7), dtype=torch.float32)
+            for idx, l in enumerate(labels):
+                label = l[0]
+                base_timestamp = self.video_timestamps[label]
+                timestamp_with_frame = base_timestamp + frame_number[idx]
+                for i, off in enumerate(self.offsets):
+                    timestamp_w_offset = timestamp_with_frame + off
+                    timestamps[(idx * self.sq_len) + i] = convert_timestamp_to_periodic(timestamp_w_offset, fps=30)
+
+            yield frames, timestamps
+
+    def __len__(self):
+        return self.pipeline.size
+
+    def compute_frame_timestamps(self, video_file_paths):
+        """
+        Compute proper timestamps for each video from oldest->newest.
+        Pre:
+            - Videos should already be in order.
+            - Videos should contain timestamp information in YYMMDD_HHMMSS
+        """
+        start = []
+        self.video_timestamps = []
+
+        for path in video_file_paths:
+            # Isolate date/time strings
+            filename = path.split("/")[-1]
+            filename = filename.casefold().strip("adcdefghijklmnopqrstuvwxyz,.;'[]{}:<>?/")
+            yearmonthday = filename.split("_")[0]
+            hourminsec = filename.split("_")[-1]
+
+            # Create substring of all info
+            date_info = []
+
+            date_info.append(int(yearmonthday[0:2]))
+            date_info.append(int(yearmonthday[2:4]))
+            date_info.append(int(yearmonthday[4:6]))
+            date_info.append(int(hourminsec[0:2]))
+            date_info.append(int(hourminsec[2:4]))
+            date_info.append(int(hourminsec[4:6]))
+
+            # Set a start date
+            if len(start) < 1:
+                for x in date_info:
+                    start.append(x)
+
+            # Calculate offset in frames
+            years = date_info[0] - start[0]
+            months = (date_info[1] - start[1]) + (years * 12)
+            days = (date_info[2] - start[2]) + (months * 30)
+            hours = (date_info[3] - start[3]) + (days * 24)
+            minutes = (date_info[4] - start[4]) + (hours * 60)
+            seconds = (date_info[5] - start[5]) + (minutes * 60)
+            frames = seconds * 30
+
+            self.video_timestamps.append(frames)
+        print(self.video_timestamps)
+
+
+class LightningDaliLoader(pl.LightningDataModule):
+    """
+    Sequenced Multiframe NVIDIA Dali powered dataloader with normalized
+    timestamps accross the whole dataset.
+
+    Args:
+        - video_file_paths: (List[str]): A list of all the paths to the data.
+        - sequence_length: int: number of total frames for multi-frame context.
+        - batch_size: int: batch size, number of multi-frame sets.
+        - num_threads: int: number of cpu(?) threads for dataloading.
+        - output_size: list[int, int]: Pipeline output image size.
+        - buffer_size: int: size of the preloaded buffer
+        - mf_distance: int: distance between sequence frames.
+        - device_id: int: device id of the cuda device to run the dataloader on.
+        - num_devices: int: number of total devices
+        - shard_id: int: id of the shard.
+    """
+
+    def __init__(
+        self,
+        video_file_paths,
+        sequence_length,
+        batch_size,
+        num_threads,
+        output_size,
+        buffer_size,
+        mf_distance,
+        num_devices,
+    ):
+        super().__init__()
+
+        self.video_file_paths = video_file_paths
+        self.sequence_length = sequence_length
+        self.batch_size = batch_size
+        self.num_threads = num_threads
+        self.output_size = output_size
+        self.buffer_size = buffer_size
+        self.mf_distance = mf_distance
+        self.num_devices = num_devices
+        self.prepare_data_per_node = False
+
+    def setup(self, stage):
+        super().setup(stage=stage)
+        self.local_rank = int(os.environ["LOCAL_RANK"])
+        self.dataset = DALIDataset(
+            self.video_file_paths,
+            self.sequence_length,
+            self.batch_size,
+            self.num_threads,
+            self.output_size,
+            self.buffer_size,
+            self.mf_distance,
+            self.num_devices,
+            self.local_rank,
+            self.local_rank,
+        )
+
+    def train_dataloader(self):
+        return DataLoader(self.dataset)
+
+    def test_dataloader(self):
+        return DataLoader(self.dataset)
+
+    def val_dataloader(self):
+        return DataLoader(self.dataset)
---
+++ data/frame_dali.py
@@ -0,0 +1,192 @@
+import torch
+import os
+import pytorch_lightning as pl
+from typing import Tuple, Dict
+from nvidia.dali import pipeline_def, Pipeline
+import nvidia.dali.fn as fn
+import nvidia.dali.types as types
+import nvidia.dali.plugin.pytorch as pydali
+from torch.utils.data import DataLoader
+from einops import rearrange
+
+from datasets.utils import convert_timestamp_to_periodic
+from datasets.frame_random_index import FrameRandomIndexModule
+
+
+@pipeline_def
+def img_pipe(
+    filelist,
+    fill,
+    name,
+    prefetch,
+    shuffle,
+    shard_id=0,
+    num_devices=1,
+):
+    j2ks, labels = fn.readers.file(
+        file_list=filelist,
+        initial_fill=fill,
+        prefetch_queue_depth=prefetch,
+        shard_id=shard_id,
+        num_shards=num_devices,
+        random_shuffle=shuffle,
+        name=name,
+    )
+
+    images = fn.experimental.decoders.image(
+        j2ks,
+        preallocate_height_hint=256,
+        preallocate_width_hint=256,
+        use_fast_idct=True,
+        device="mixed",
+    )
+
+    reorder = fn.transpose(images, perm=[2, 0, 1])
+    scale = types.Constant(255)
+    normalized = reorder / scale
+    return normalized, labels
+
+
+class DALIDataset(torch.utils.data.IterableDataset):
+    """
+    Iterator Style pytorch dataset that uses a NVIDIA DALI pipeline to load video data
+    quickly with gpu acceleration. Also provides multi-frame context.
+
+    Args:
+        - video_file_paths: (List[str]): A list of all the paths to the data.
+        - sequence_length: int: number of total frames for multi-frame context.
+        - batch_size: int: batch size, number of multi-frame sets.
+        - num_threads: int: number of cpu(?) threads for dataloading.
+        - output_size: list[int, int]: Pipeline output image size.
+        - buffer_size: int: size of the preloaded buffer
+        - mf_distance: int: distance between sequence frames.
+        - device_id: int: device id of the cuda device to run the dataloader on.
+        - num_devices: int: number of total devices
+        - shard_id: int: id of the shard.
+    """
+
+    def __init__(
+        self,
+        filelist,
+        periodic,
+        batch_size,
+        num_threads,
+        fill,
+        prefetch,
+        shuffle,
+        shard_id=0,
+        num_devices=1,
+    ):
+        super().__init__()
+        self.name = f"Reader{shard_id}"
+        pipe = img_pipe(
+            filelist=filelist,
+            fill=fill,
+            prefetch=prefetch,
+            shuffle=shuffle,
+            name=self.name,
+            shard_id=shard_id,
+            num_devices=num_devices,
+            batch_size=batch_size,
+            num_threads=num_threads,
+            device_id=shard_id,
+        )
+        self.pipeline = pydali.DALIGenericIterator(pipe, output_map=["frames", "labels"], reader_name=self.name)
+        self.periodic
+
+    def __iter__(self):
+        for data in enumerate(self.pipeline):
+            frames, labels = (
+                data[1][0]["frames"],
+                data[1][0]["labels"],
+            )
+
+            timestamps_list = [convert_timestamp_to_periodic(int(l[0]), fps=30) for l in labels]
+            timestamps = torch.stack(timestamps_list)
+
+            yield frames, timestamps
+
+    def __len__(self):
+        return self.pipeline.size
+
+
+class DaliImage(pl.LightningDataModule):
+    """
+    Sequenced Multiframe NVIDIA Dali powered dataloader with normalized
+    timestamps accross the whole dataset.
+
+    Args:
+        - filelist: list of files w labels
+        - periodic: periodic timestamp embeddings yes or no
+        - batch_size: batch size
+        - fill: how many imgs to fill in queue
+        - prefetch: prefetch queue depth
+        - num_devices: int: number of total devices
+        - shard_id: int: id of the shard.
+    """
+
+    def __init__(
+        self,
+        filelist: str,
+        periodic: bool,
+        batch_size: int,
+        num_threads: int,
+        fill: int,
+        prefetch: int,
+        num_devices: int,
+        val_dataloader: Dict,
+    ):
+        super().__init__()
+        self.filelist = filelist
+        self.periodic = periodic
+        self.batch_size = batch_size
+        self.fill = fill
+        self.prefetch = prefetch
+        self.num_threads = num_threads
+        self.num_devices = num_devices
+        self.val_dict = val_dataloader
+
+    def setup(self, stage):
+        super().setup(stage=stage)
+        self.local_rank = int(os.environ["LOCAL_RANK"])
+
+        self.name = f"Reader{self.local_rank}"
+        pipe = img_pipe(
+            filelist=self.filelist,
+            fill=self.fill,
+            prefetch=self.prefetch,
+            shuffle=True,
+            name=self.name,
+            shard_id=self.local_rank,
+            num_devices=self.num_devices,
+            batch_size=self.batch_size,
+            num_threads=self.num_threads,
+            device_id=self.local_rank,
+        )
+
+        class LightningWrapper(pydali.DALIGenericIterator):
+            def __init__(self, periodic, *kargs, **kvargs):
+                super().__init__(*kargs, **kvargs)
+                self.periodic = periodic
+
+            def __next__(self):
+                out = super().__next__()
+                labels = out[0]["labels"]
+                frames = out[0]["frames"]
+
+                if self.periodic:
+                    label_list = [convert_timestamp_to_periodic(label) for label in labels]
+                    labels = torch.stack(label_list)
+
+                return frames, labels
+
+        self.loader = LightningWrapper(self.periodic, pipe, output_map=["frames", "labels"], reader_name=self.name)
+
+    def train_dataloader(self):
+        return self.loader
+
+    def test_dataloader(self):
+        return self.loader
+
+    def val_dataloader(self):
+        return FrameRandomIndexModule(**self.val_dict).val_dataloader()
