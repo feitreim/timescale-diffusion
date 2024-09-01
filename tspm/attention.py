96b722e9b7ba6027afd4942d6d1c1e3d863a24aa
finn eitreim <feitreim0@gmail.com>
2024-08-31 19:42:33 -0700

    Optimize AttnBlock with combined KV projection and add MLP layers

--- tspm/attention.py
+++ tspm/attention.py
@@ -17,9 +17,13 @@ class AttnBlock(nn.Module):
     def __init__(self, e_dim):
         super().__init__()
         self.q_proj = torch.nn.Parameter(torch.randn(1, e_dim, e_dim), requires_grad=True)
-        self.k_proj = torch.nn.Parameter(torch.randn(1, e_dim, e_dim), requires_grad=True)
-        self.v_proj = torch.nn.Parameter(torch.randn(1, e_dim, e_dim), requires_grad=True)
+        self.kv_proj = torch.nn.Parameter(torch.randn(1, e_dim, e_dim * 2), requires_grad=True)
         self.attn_proj = torch.nn.Parameter(torch.randn(1, e_dim, e_dim), requires_grad=True)
+        self.mlp_up = torch.nn.Parameter(torch.randn(1, e_dim, e_dim * 2), requires_grad=True)
+        # S = 256 hard coded rn
+        self.mlp_bias = torch.nn.Parameter(torch.randn(1, e_dim * 2, e_dim * 2), requires_grad=True)
+        self.mlp_down = torch.nn.Parameter(torch.randn(1, e_dim * 2, e_dim), requires_grad=True)
+
         self.e_dim = e_dim
         self.norm = nn.LayerNorm(e_dim)
 
@@ -30,15 +34,20 @@ class AttnBlock(nn.Module):
             -- t => (B, e_dim)
         """
         B, S, shape = x.shape[0], x.shape[1], x.shape
-        x = x.view(B, -1, self.e_dim)
+        x = x.reshape(B, -1, self.e_dim)
         t = t.unsqueeze(1).expand(-1, S, -1)
         q = self.norm(x @ self.q_proj)
-        k = self.norm(t @ self.k_proj)
-        v = self.norm(t @ self.v_proj)
+        # do both k and v at the same time in one mm to speed up
+        # in self attn you can actually do all 3 at once
+        kv = t @ self.kv_proj
+        k, v = torch.split(kv, self.e_dim, dim=-1)
+        k, v = self.norm(k), self.norm(v)
         # take the attention map (phi) and add it to (x)
         r = f.scaled_dot_product_attention(q, k, v) @ self.attn_proj
-        x = x + nn.functional.tanh(r)
-        return x.view(*shape)
+        print(r.shape)
+        r = f.gelu((r @ self.mlp_up) + self.mlp_bias)
+        x = x + (r @ self.mlp_down)
+        return x.reshape(*shape).to(memory_format=torch.channels_last)
 
 
 class AttnDown(nn.Module):
